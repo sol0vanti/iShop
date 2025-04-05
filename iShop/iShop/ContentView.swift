2bad7a1667d1db0f48c7d525200a97f9b1ca9bf2
sol0vanti <ialexballa@gmail.com>
2025-04-05 20:14:42 +0300

    5 Apr - Style init view

--- iShop/iShop/ContentView.swift
+++ iShop/iShop/ContentView.swift
@@ -10,9 +10,31 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    @State private var emailAddress: String = ""
 
     var body: some View {
-        Text("Hello, world")
+        NavigationStack {
+            VStack {
+                TextField("Email", text: $emailAddress)
+                    .textFieldStyle(.roundedBorder)
+                    .font(.headline)
+                    .padding(.horizontal, 25)
+                
+                Button{
+                    
+                } label: {
+                    Label("Sign in or sign up", systemImage: "shield.lefthalf.filled")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 25)
+                        .font(.headline)
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.horizontal, 25)
+                .padding(.vertical, 15)
+                //            ============ Nav Bar Styles  ============
+                .navigationTitle("iShop")
+            }
+        }
     }
 }
 
